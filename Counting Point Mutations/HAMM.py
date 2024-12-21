54d181c7f62e5b860486c6b443d08e723aa99224
Shaswata Roy <shaswataroy@Shaswatas-MacBook-Air.local>
2024-12-20 20:53:39 -0500

    Counting Point Mutations

---
+++ Counting Point Mutations/HAMM.py
@@ -0,0 +1,19 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--file', type=str, default="")
+args = parser.parse_args()
+
+
+f = open(args.file,"r")
+s = f.readline().strip()
+t = f.readline().strip()
+
+hamming_distance = 0
+
+for indx in range(len(s)):
+    if s[indx] != t[indx]:
+        hamming_distance += 1
+
+print(hamming_distance)
+
